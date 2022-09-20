66e1d967eecfafeb8db7fb4392037180205f367b
RainbowMango <qdurenhongcai@gmail.com>
2022-09-20 15:38:23 +0800

    Use controller-gen to generates the deepcopy file

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -8,18 +8,14 @@ set -o pipefail
 REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${REPO_ROOT}"
 
-GO111MODULE=on go install k8s.io/code-generator/cmd/deepcopy-gen
-GO111MODULE=on go install k8s.io/code-generator/cmd/register-gen
-GO111MODULE=on go install k8s.io/code-generator/cmd/client-gen
-GO111MODULE=on go install k8s.io/code-generator/cmd/lister-gen
-GO111MODULE=on go install k8s.io/code-generator/cmd/informer-gen
+GO111MODULE=on go install "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.8.0"
+GO111MODULE=on go install "k8s.io/code-generator/cmd/register-gen"
+GO111MODULE=on go install "k8s.io/code-generator/cmd/client-gen"
+GO111MODULE=on go install "k8s.io/code-generator/cmd/lister-gen"
+GO111MODULE=on go install "k8s.io/code-generator/cmd/informer-gen"
 
-echo "Generating with deepcopy-gen"
-deepcopy-gen \
-  --go-header-file hack/boilerplate/boilerplate.generatego.txt \
-  --input-dirs=sigs.k8s.io/about-api/pkg/apis/v1alpha1 \
-  --output-package=sigs.k8s.io/about-api/pkg/apis/v1alpha1 \
-  --output-file-base=zz_generated.deepcopy
+echo "Generating deep copy files with controller-gen"
+controller-gen object:headerFile="hack/boilerplate/boilerplate.generatego.txt" paths="./pkg/apis/v1alpha1/..."
 
 echo "Generating with register-gen"
 register-gen \
